d6b61507a73a06404ebc5eab1c60eeac9852d5bd
NicDominguez <NicDominguez@users.noreply.github.com>
2020-01-18 15:54:49 -0800

    added explanatory comments

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Sets view engine to PUG
 app.set('view engine', 'pug');
 
- // Set static files
+ // Set static files folder to public
  app.use('/static', express.static('public'))
 
 
- // Import routes
+ // Import and use routes from routes files
  const mainRoutes = require('./routes/main');
  const projectRoutes = require('./routes/projects')
  
@@ -22,13 +23,14 @@ app.set('view engine', 'pug');
     next(err);
  });
 
-
+ // Uses error when no route exists
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
     res.render('error', err);
  });
 
+ // Sets port to 3000
  app.listen(3000, () => {
     console.log('The application is running on localhost:3000');
 });
--- routes/projects.js
+++ routes/projects.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json')
 
+
+// Dynamically renders project pug file depending on project route destination
 router.get('/:id', (req, res) => {
     res.render('project', {
         projects, req 
